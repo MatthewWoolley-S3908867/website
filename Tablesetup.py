13589e897808947b17b38589be21f2494ce42313
MatthewWoolley-S3908867 <mattwooll.a@gmail.com>
2023-03-21 23:04:13 +1100

    first commit

---
+++ Tablesetup.py
@@ -0,0 +1,54 @@
+import boto3
+import json
+def makemusictable():
+
+    table = dynamodb.create_table(
+    TableName='music',
+        KeySchema=[
+            {
+                'AttributeName': 'Title',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'Artist',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'Title',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Artist',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+
+    )
+    print(table)
+
+def filltable():
+    dynamodb = boto3.client('dynamodb')
+    file = open('a1.json', 'r')
+    data = json.load(file)['songs']
+    for item in data:
+        dynamodb.put_item(
+            TableName='music',
+            Item={
+                    'Title': {'S': item['title']},
+                    'Artist': {'S': item['artist']},
+                    'Year': {'S': item['year']},
+                    'Web_url': {'S': item['web_url']},
+                    'Img_url': {'S': item['img_url']}
+                }
+        )
+    print("full")
+
+#makemusictable()
+#print("tablemade")
+filltable()
